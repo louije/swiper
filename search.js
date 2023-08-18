48eccd0a763c3b12104ac9d8884d0aa78ce517ec
Louis-Jean Teitelbaum <ljt@meidosem.com>
2023-08-18 20:20:11 +0200

    fix: add apiKey to every request

--- search.js
+++ search.js
@@ -1,10 +1,9 @@
 import { MeiliSearch } from "meilisearch";
 
-const apiKey = process.env.MEILI_MASTER_KEY;
 
 export async function search(query, coords) {
+  const apiKey = process.env.MEILI_MASTER_KEY;
   const client = new MeiliSearch({ host: process.env.MEILI_URL, apiKey });
-  console.log("host", process.env.MEILI_URL, "client", client);
 
   let options = {};
   if (coords && coords.lat && coords.lng) {
